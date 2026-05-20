1fdb7b14b26697161f9dc441b1059762ee13eee2
Graham Pugh <5802725+grahampugh@users.noreply.github.com>
2026-05-20 15:33:38 +0100

    add messages when running these stub scripts

--- download-eas.sh
+++ download-eas.sh
@@ -8,4 +8,11 @@
 # ./jamfuploader-run.sh read --type mobile_device_extension_attribute --name "EA NAME" --output /Users/Shared/Jamf/JamfUploader
 # --------------------------------------------------------------------------------
 
+echo "Please use the following command to download EAs:"
+echo ""
+echo "./jamfuploader-run.sh read --type computer_extension_attribute --name \"EA NAME\" --output /Users/Shared/Jamf/JamfUploader"
+echo "or"
+echo "./jamfuploader-run.sh read --type mobile_device_extension_attribute --name \"EA NAME\" --output /Users/Shared/Jamf/JamfUploader"
+echo ""
+
 exit 0
